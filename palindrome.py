f463f8e63ad5031a2ef4460a527f7b427895f610
hoangmatthew1999 <hoangmatthew1999@gmail.com>
2019-09-15 22:48:09 -0700

    completed answer

--- palindrome.py
+++ palindrome.py
@@ -1,28 +1,28 @@
-def palindrome(x):
-    if(x < 0):
-        return False
-    elif(x > 0 and x < 10):
-        return True
-    else:
-        smaller = 10
-        digits = []
-        digits.append(x % 10)
-        while(x >= smaller ):
-            digit = x // smaller
-            pushing = digit % 10
-            digits.append(pushing)
-            smaller = smaller * 10
+def isPalindrome( x):
+        if(x < 0):
+            return False
+        elif(x > 0 and x < 10):
+            return True
+        else:
+            smaller = 10
+            digits = []
+            digits.append(x % 10)
+            while(x >= smaller ):
+                digit = x // smaller
+                pushing = digit % 10
+                digits.append(pushing)
+                smaller = smaller * 10
             print(digits)
-        i = 0
-        j = len(digits) - 1
-        for i in range ( len(digits) ):
-            if(digits[i] == digits[j] ):
-                print( digits[i], digits[j] )
-                i = i + 1
-                j = j - 1
-            else:
-                break 
-                return False
-        return True
-print( palindrome(10) )
-
+            i = 0
+            j = len(digits) - 1
+            for i in range(len(digits)):
+                print(digits[i],digits[j],"printing")
+                if(digits[i] == digits[j] ):
+                    print("in if")
+                    i = i + 1
+                    j = j - 1
+                else:
+                    print("in else")
+                    return False
+                    break 
+print( isPalindrome(10)  )
