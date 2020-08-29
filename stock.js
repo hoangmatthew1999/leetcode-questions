d7a14ae75b44f1d5299e39d36d35a20d06ee4c06
Matthew Hoang <hoangmatthew1999@gmail.com>
2020-08-29 06:29:34 -0700

    create stock.js

---
+++ stock.js
@@ -0,0 +1,31 @@
+ if (min == 99){
+//             if(i >= 1 && prices[i] > prices[i - 1]){
+//                     min = prices[i-1];console.log(min);}       
+//             }
+      
+//         if (prices[i] > max){
+//             max  = prices[i];
+//             maxIndex = i;
+//             console.log(max);
+//             if (min == 99999){    
+//                 if(i >= 1 && prices[i] > prices[i - 1]){
+//                     min = prices[i-1];console.log(min)}       
+//             }
+//             if(min != 99999){
+//                 if(max - min >= difference){
+//                     difference = max - min;
+//                 }
+//             }
+//         }
+        
+//         else if(prices[i] < min){
+//             min = prices[i];
+//             // console.log(min,"else");
+//             if(maxIndex > i){max = 0;}
+//             max = 0;
+//             if(max - min > difference){
+//                 difference = max - min;
+//             }
+//         }
+        
+//     }
