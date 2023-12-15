e80a8429145ba9d085b60240883bd6003e543daa
Matthew Hoang <hoangmatthew1999@gmail.com>
2023-12-14 19:27:51 -0800

    figured out how to perform the loop properly

--- subsetPython2.py
+++ subsetPython2.py
@@ -1,24 +1,67 @@
-class Solution(object):
-    def subsets(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        currentLength = 3
+ currentLength = 3
         returnArray = []
         withoutLastElementArray = []
+        # for i in range(currentLength - 1):
+        #     withoutLastElementArray.append(nums[i])
+        # lastElementIndex = 2
+        # for iterations in range(len(nums) - lastElementIndex):
+        #     if len(withoutLastElementArray) - 1 < lastElementIndex:
+        #         withoutLastElementArray.append(nums[lastElementIndex+iterations])
+        #         arrayTemp = withoutLastElementArray[:]
+        #         returnArray.append(arrayTemp)
+        #     else:
+        #         withoutLastElementArray[len(withoutLastElementArray)-1] = nums[lastElementIndex+iterations]
+        #         arrayTemp = withoutLastElementArray[:]
+        #         returnArray.append(arrayTemp)
+
+        # withoutLastElementArray.pop()
+        # withoutLastElementArray.pop()
+        # lastElementIndex = 3
+        # withoutLastElementArray.append(nums[lastElementIndex-1])
+        # for i in range(len(nums) - lastElementIndex):
+        #     # withoutLastElementArray[lastElementIndex-1] = nums[lastElementIndex+i]
+        #     if len(withoutLastElementArray)  < lastElementIndex:
+        #         withoutLastElementArray.append(nums[lastElementIndex+i])
+        #         arrayTemp = withoutLastElementArray[:]
+        #         returnArray.append(arrayTemp)
+        #     else:
+        #         withoutLastElementArray[len(withoutLastElementArray)-1] = nums[lastElementIndex+i]
+        #         arrayTemp = withoutLastElementArray[:]
+        #         returnArray.append(arrayTemp)
+                
+        lastElementIndex = 2
         for i in range(currentLength - 1):
             withoutLastElementArray.append(nums[i])
-        lastElementIndex = 2
-        for iterations in range(len(nums) - lastElementIndex):
-            if len(withoutLastElementArray) - 1 < lastElementIndex:
-                withoutLastElementArray.append(nums[lastElementIndex+iterations])
+
+
+        for j in range(len(nums) - lastElementIndex):
+          for i in range(len(nums) - lastElementIndex):
+            if len(withoutLastElementArray) < currentLength:# if the length of WLE is 2 then append so = 3
+              withoutLastElementArray.append(nums[lastElementIndex+i])
+              print(withoutLastElementArray)
+              arrayTemp = withoutLastElementArray[:]
+              returnArray.append(arrayTemp)
             else:
-                withoutLastElementArray[lastElementIndex] = nums[lastElementIndex+iterations]
-                arrayTemp = withoutLastElementArray[:]
-                returnArray.append(arrayTemp)
+              withoutLastElementArray[len(withoutLastElementArray)-1] = nums[lastElementIndex+i]
+              arrayTemp = withoutLastElementArray[:]
+              returnArray.append(arrayTemp)
+          
+          lastElementIndex = lastElementIndex + 1
+          withoutLastElementArray.pop()
+          withoutLastElementArray[len(withoutLastElementArray)-1] = nums[lastElementIndex-1]
 
-        return returnArray
+        print(withoutLastElementArray)
 
+
+          
+
+
+
+
+
+
+            
 
         
+
+        return returnArray
