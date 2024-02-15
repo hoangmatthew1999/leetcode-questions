17fa055266950478fd740c96574035527f66b352
Matthew Hoang <hoangmatthew1999@gmail.com>
2024-02-14 19:59:32 -0800

    solved solution

--- dailyTemp.py
+++ dailyTemp.py
@@ -1,34 +1,27 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        valuesVisitedArray = []
-        returnArray = []
-        offset = 0
-        counter = 0
-        i = len(temperatures)-1
-        e = temperatures[i]
-        if i == len(temperatures)-1:# first elem 73
-            valuesVisitedArray.insert(0,temperatures[len(temperatures)-1])
-            returnArray.insert(0,0)
+        returnArray = [0] * len(temperatures)
+        valuesVisitedStack = []
+        indexVisitedStack = []
 
-        for index in range( 1,len(temperatures) ):
-            i = i - 1
-            e = temperatures[i]
-            counter = 1
-            j = 0
-            if e < valuesVisitedArray[0]:# elem 75
-                returnArray.insert(0,1)
-                valuesVisitedArray.insert(0,e)
-            else:
-                for elem in range(len(valuesVisitedArray)):
-                    if e < valuesVisitedArray[j]:
-                        returnArray.insert(0,counter)
-                        valuesVisitedArray.insert(0,e)
-                        break
-                    else:
-                        j = j + 1
-                        counter = counter + 1
-                if len(temperatures) - i > len(valuesVisitedArray):# if you have not added the value already
-                    valuesVisitedArray.insert(0,e)
-                    returnArray.insert(0,0)
+        i = 0
+        valuesVisitedStack.append(i)
+        indexVisitedStack.append(temperatures[i])
+
+        for loopingArray in range(len(temperatures) - 1):
+            i = i + 1
+            valuesVisitedStack.append(i)
+            indexVisitedStack.append(temperatures[i])
+            top = len(valuesVisitedStack)-1
+            while indexVisitedStack[top-1] < indexVisitedStack[top]:
+                diff = valuesVisitedStack[top] - valuesVisitedStack[top-1]
+                returnArray[valuesVisitedStack[top-1]] = diff
+                valuesVisitedStack.pop(top-1)
+                indexVisitedStack.pop(top-1)
+                top = len(valuesVisitedStack)-1
+        
+        if i == len(temperatures) -1:
+            returnArray[len(temperatures)-1] = 0
+        
 
         return returnArray
