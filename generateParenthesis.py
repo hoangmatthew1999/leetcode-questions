d37ecdbb73cbab0e7e880e46256d4835779078ac
Matthew Hoang <hoangmatthew1999@gmail.com>
2024-02-20 18:45:22 -0800

    Update generateParenthesis.py

--- generateParenthesis.py
+++ generateParenthesis.py
@@ -15,12 +15,12 @@ class Solution:
         if OpenBracketNum > 0:
             eachString = currentLevel[0] + "("
             returnArray.append(eachString)
-            OpenBracketDB[eachString] = n-1
+            OpenBracketDB[eachString] = n-1-1
             CloseBracketDB[eachString] = n
         if CloseBracketNum > 0 and currentLevel[0] == "(":
             eachString = currentLevel[0] + ")"
             returnArray.append(eachString)
-            OpenBracketDB[eachString] = n
+            OpenBracketDB[eachString] = n-1
             CloseBracketDB[eachString] = n-1
         currentLevel.pop(0)
 
@@ -28,8 +28,41 @@ class Solution:
         if OpenBracketDB[targetString] > 0:
             eachString = targetString + "("
             returnArray.append(eachString)
-            OpenBracketDB[eachString] = OpenBracketDB[targetString] + 1
+            OpenBracketDB[eachString] = OpenBracketDB[targetString] - 1
             CloseBracketDB[eachString] = n
+        if CloseBracketDB[targetString] > 0:
+            eachString = targetString + ")"
+            returnArray.append(eachString)
+            OpenBracketDB[eachString] = OpenBracketDB[targetString] 
+            CloseBracketDB[eachString] = CloseBracketDB[targetString] - 1
+        OpenBracketDB.pop(targetString)
+        CloseBracketDB.pop(targetString)
+
+        targetString = currentLevel[1]
+        if OpenBracketDB[targetString] > 0:
+            eachString = targetString + "("
+            returnArray.append(eachString)
+            OpenBracketDB[eachString] = OpenBracketDB[targetString] - 1
+            CloseBracketDB[eachString] = n
+        if CloseBracketDB[targetString] > 0 and targetString[-1] == "(":
+            eachString = targetString + ")"
+            returnArray.append(eachString)
+            OpenBracketDB[eachString] = OpenBracketDB[targetString] 
+            CloseBracketDB[eachString] = CloseBracketDB[targetString] - 1
+        OpenBracketDB.pop(targetString)
+        CloseBracketDB.pop(targetString)
+
+        targetString = currentLevel[2]
+        if CloseBracketDB[targetString] > 0:
+            eachString = targetString + ")"
+            returnArray.append(eachString)
+            OpenBracketDB[eachString] = OpenBracketDB[targetString] 
+            CloseBracketDB[eachString] = CloseBracketDB[targetString] - 1
+
+
+
+
+        
         
 
         print(OpenBracketDB,"seperator", CloseBracketDB)
