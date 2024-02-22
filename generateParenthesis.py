ed5ad20d2ee0102fe322f3387e2d8aadb1d6e3ac
Matthew Hoang <hoangmatthew1999@gmail.com>
2024-02-21 17:13:17 -0800

    finish hardcoded solution

--- generateParenthesis.py
+++ generateParenthesis.py
@@ -4,6 +4,88 @@ class Solution:
             return  ["()"]
 
         returnArray = []
+        currentLevel = []
+        OpenBracketDB = dict()
+        CloseBracketDB = dict()
+
+        OpenBracketDB["("] = n-1
+        CloseBracketDB["("] = n
+        targetString = "("
+        if OpenBracketDB[targetString]> 0:#length 2
+            eachString = targetString + "("
+            returnArray.append(eachString)
+            OpenBracketDB[eachString] = OpenBracketDB[targetString] - 1
+            CloseBracketDB[eachString] = n
+        if CloseBracketDB[targetString] > 0 and targetString[-1] == "(":
+            eachString = targetString + ")"
+            returnArray.append(eachString)
+            OpenBracketDB[eachString] = OpenBracketDB[targetString]
+            CloseBracketDB[eachString] = CloseBracketDB[targetString] - 1
+        OpenBracketDB.pop(targetString)
+        CloseBracketDB.pop(targetString)
+
+        currentLevel = returnArray
+        returnArray = []
+        targetString = currentLevel[0]#length 3
+        if OpenBracketDB[targetString] > 0:
+            eachString = targetString + "("
+            returnArray.append(eachString)
+            OpenBracketDB[eachString] = OpenBracketDB[targetString] - 1
+            CloseBracketDB[eachString] = CloseBracketDB[targetString]
+        if CloseBracketDB[targetString] > 0 and targetString[-1] == "(":
+            eachString = targetString + ")"
+            returnArray.append(eachString)
+            OpenBracketDB[eachString] = OpenBracketDB[targetString] 
+            CloseBracketDB[eachString] = CloseBracketDB[targetString] - 1
+        OpenBracketDB.pop(targetString)
+        CloseBracketDB.pop(targetString)
+
+        targetString = currentLevel[1]
+        if OpenBracketDB[targetString] > 0:
+            eachString = targetString + "("
+            returnArray.append(eachString)
+            OpenBracketDB[eachString] = OpenBracketDB[targetString] - 1
+            CloseBracketDB[eachString] = OpenBracketDB[targetString]
+        if CloseBracketDB[targetString] > 0 and targetString[-1] == "(":
+            eachString = targetString + ")"
+            returnArray.append(eachString)
+            OpenBracketDB[eachString] = OpenBracketDB[targetString] 
+            CloseBracketDB[eachString] = CloseBracketDB[targetString] - 1
+        OpenBracketDB.pop(targetString)
+        CloseBracketDB.pop(targetString)
+        currentLevel = returnArray
+        returnArray = []
+
+        targetString = currentLevel[0]#length 4
+        if CloseBracketDB[targetString] > 0:#band aided solution possible 
+            eachString = targetString + ")"
+            returnArray.append(eachString)
+            OpenBracketDB[eachString] = OpenBracketDB[targetString] 
+            CloseBracketDB[eachString] = CloseBracketDB[targetString] - 1
+
+        targetString = currentLevel[1]
+        if CloseBracketDB[targetString] > 0:#band aided solution possible 
+            eachString = targetString + ")"
+            returnArray.append(eachString)
+            OpenBracketDB[eachString] = OpenBracketDB[targetString] 
+            CloseBracketDB[eachString] = CloseBracketDB[targetString] - 1
+
+        '''
+        possible for ) able to fit with ( being -1 
+        ex (()
+        '''
+
+        print(OpenBracketDB,"seperator", CloseBracketDB)
+        return returnArray
+
+        
+
+
+
+
+            return  ["()"]
+
+        returnArray = []
         OpenBracketNum = n - 1
         CloseBracketNum = n
         returnArray.append("(")
