b736b32e3f88661d81f50547330b6f8669d344b1
Matthew Hoang <hoangmatthew1999@gmail.com>
2024-04-10 16:10:17 -0700

    just need to turn into final solution

--- TopK.py
+++ TopK.py
@@ -76,39 +76,120 @@ class Solution(object):
             valueToRemove = valueToKeyDB[returnArrayMin]
             valueToRemove = valueToRemove[0]
             indexToRemove = returnArrayDB[valueToRemove]
-            returnArrayDB.pop(valueToRemove)
             returnArray[indexToRemove] = nums[i]
             returnArrayDB[nums[i]] = indexToRemove
+            returnArrayDB.pop(valueToRemove)
+            valueToKeyDB.pop(1)
+            valueToKeyDB[2].append(3)
+            returnArrayMin = next ( iter(valueToKeyDB) )
+
+        i = 5
+        if nums[i] not in freqDB:
+            freqDB[nums[i]] = 1
+        elif nums[i] in freqDB:
+            freqDB[nums[i]] = freqDB[nums[i]] + 1
+        if nums[i] in returnArrayDB and freqDB[nums[i]] > returnArrayMin:
+            valueToKeyDB[3] = [nums[i]]
+        elif freqDB[nums[i]] > returnArrayMin and nums[i] not in returnArrayDB:
+            valueToRemove = valueToKeyDB[returnArrayMin]
+            valueToRemove = valueToRemove[0]
+            indexToRemove = returnArrayDB[valueToRemove]
+            returnArray[indexToRemove] = nums[i]
+            returnArrayDB[nums[i]] = indexToRemove
+            returnArrayDB.pop(valueToRemove)
+            valueToKeyDB.pop(1)
+            valueToKeyDB[2].append(3)
+            returnArrayMin = next ( iter(valueToKeyDB) )
+            
+        i = 6
+        if nums[i] not in freqDB:
+            freqDB[nums[i]] = 1
+        elif nums[i] in freqDB:
+            freqDB[nums[i]] = freqDB[nums[i]] + 1
+        if nums[i] in returnArrayDB and freqDB[nums[i]] > returnArrayMin:
+            valueToKeyDB[3] = [nums[i]]
+        elif freqDB[nums[i]] > returnArrayMin and nums[i] not in returnArrayDB:
+            valueToRemove = valueToKeyDB[returnArrayMin]
+            valueToRemove = valueToRemove[0]
+            indexToRemove = returnArrayDB[valueToRemove]
+            returnArray[indexToRemove] = nums[i]
+            returnArrayDB[nums[i]] = indexToRemove
+            returnArrayDB.pop(valueToRemove)
+            valueToKeyDB.pop(1)
+            valueToKeyDB[2].append(3)
+            returnArrayMin = next ( iter(valueToKeyDB) )
+        # if freqDB[nums[i]] > returnArrayMin:
+        #     removingElem = valueToKeyDB[1]
+        #     returnArray[0] = nums[i]
+        #     returnArrayMin = 2
+        i = 7
+        if nums[i] not in freqDB:
+            freqDB[nums[i]] = 1
+        elif nums[i] in freqDB:
+            freqDB[nums[i]] = freqDB[nums[i]] + 1
+        if nums[i] in returnArrayDB and freqDB[nums[i]] > returnArrayMin:
+            valueToKeyDB[3] = [nums[i]]
+            print("asd")
+        elif freqDB[nums[i]] > returnArrayMin and nums[i] not in returnArrayDB:
+            print("qwe")
+        
+        i = 8
+        if nums[i] not in freqDB:
+            freqDB[nums[i]] = 1
+        elif nums[i] in freqDB:
+            freqDB[nums[i]] = freqDB[nums[i]] + 1
+        if nums[i] in returnArrayDB and freqDB[nums[i]] > returnArrayMin:
+            # valueToKeyDB[3] = [nums[i]]
+            print("asd")
+        elif freqDB[nums[i]] > returnArrayMin and nums[i] not in returnArrayDB:
+            print("qwe")
+
+        i = 9
+        if nums[i] not in freqDB:
+            freqDB[nums[i]] = 1
+        elif nums[i] in freqDB:
+            freqDB[nums[i]] = freqDB[nums[i]] + 1
+        if nums[i] in returnArrayDB and freqDB[nums[i]] > returnArrayMin:
+            # valueToKeyDB[3] = [nums[i]]
+            print("asd")
+        elif freqDB[nums[i]] > returnArrayMin and nums[i] not in returnArrayDB:
+            valueToRemove = valueToKeyDB[returnArrayMin][0]
+            indexToRemove = returnArrayDB[valueToRemove]
+            returnArrayDB.pop(valueToRemove)
+            returnArrayDB[nums[i]] = indexToRemove
+            returnArray[indexToRemove] = nums[i]
+            valueToKeyDB[2] = []
+            if len(valueToKeyDB[3]) == 0:
+                valueToKeyDB[2] = [nums[i]]
+            else:
+                valueToKeyDB[3].append(nums[i])
 
             
 
-
-        # valueToKeyDB[2].append(nums[i])
-        # valueToKeyDB[1].pop(-1)
-
-
-        # i = 5
-        # if nums[i] not in freqDB:
-        #     freqDB[nums[i]] = 1
-        # elif nums[i] in freqDB:
-        #     freqDB[nums[i]] = freqDB[nums[i]] + 1
-        # if freqDB[nums[i]] > returnArrayMin:
-        #     removingElem = valueToKeyDB[1]
-        #     returnArray[0] = nums[i]
-        #     returnArrayMin = 2
-        # if freqDB[nums[i]] > returnArrayMax:
-        #     returnArrayMax = freqDB[nums[i]]
         
 
-        # i = 6
-        # if nums[i] not in freqDB:
-        #     freqDB[nums[i]] = 1
-        # elif nums[i] in freqDB:
-        #     freqDB[nums[i]] = freqDB[nums[i]] + 1
+        
+
+
+        
+
+        print(" ")
+        print(returnArrayMin)
+        print(freqDB)
+        print(valueToKeyDB)
+        print(returnArrayDB)
+
+        return returnArray
+
+        
         # if freqDB[nums[i]] > returnArrayMin:
         #     removingElem = valueToKeyDB[1]
         #     returnArray[0] = nums[i]
         #     returnArrayMin = 2
+       
+        
+
+        
         
         # i = 7# 2
         # if nums[i] not in freqDB:
@@ -132,14 +213,6 @@ class Solution(object):
         #     returnArray.pop()
         #     returnArray.append(nums[i])
 
-        # print(returnArrayMin)
-        # print(freqDB)
-        print(valueToKeyDB)
-        print(returnArrayDB)
-
-        return returnArray
-
-        
         # i = 1
         # if nums[i] not in freqDB:
         #     freqDB[nums[i]] = 1
